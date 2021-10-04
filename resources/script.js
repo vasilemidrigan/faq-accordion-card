d889ab453aca539faa56515b43dd46c8656422ba
Vasile Midrigan <111rigan111@gmail.com>
2021-10-04 04:25:33 +0100

    implementing dropdown answers, bold selected question, arrow animation, etc

---
+++ resources/script.js
@@ -0,0 +1,18 @@
+// Strict mode
+"use strict";
+// showHide() function
+// (showing and hidding our answers while clicking on the questions)
+const showHide = (answerID, arrowID, askID) => {
+  const element = document.getElementById(answerID);
+  const arrow = document.getElementById(arrowID);
+  const ask = document.getElementById(askID);
+  if (element.style.display === "block") {
+    ask.style.fontWeight = "unset";
+    element.style.display = "none";
+    arrow.style.transform = "rotate(0.5turn)";
+  } else {
+    ask.style.fontWeight = "bold";
+    element.style.display = "block";
+    arrow.style.transform = "rotate(1turn)";
+  }
+};
